ac289862b82137c0941166e20e12178d9d7f89db
yellowsea <yunhai0644@163.com>
2023-07-30 14:06:51 +0800

    feat✨: active bot and user message

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Configuration, OpenAIApi } from "openai"
-
 import dotenv from "dotenv";
-dotenv.config();
+import readlineSync from 'readline-sync'
 
+dotenv.config();
 
 const openAi = new OpenAIApi(
   new Configuration({
@@ -11,17 +11,53 @@ const openAi = new OpenAIApi(
   })
 );
 
-(async () => {
-  const chatCompletion = await openAi.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "user",
-        content: "vuejs的作者是谁?",
-      },
-    ],
-  });
+const messages: { role: 'user' | 'assistant'; content: string }[] = [];
 
-  const { choices } = chatCompletion.data;
-  console.log(choices[0].message?.content);
+(async () => {
+
+  while (true) {
+    // 获取用户的输入
+    const userInput = readlineSync.question("you:")
+
+
+    // 判断用户的输入
+    if (userInput.toLowerCase() === 'exit') {
+      console.log('退出')
+      return
+    } else if (userInput.toLowerCase() === 'quit') {
+      console.log('退出')
+      return
+    }
+
+
+    // 将用户的输入添加到消息列表中
+    messages.push({
+      role: 'user',
+      content: userInput
+    })
+
+    const chatCompletion = await openAi.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages,
+      // messages: [
+      //   {
+      //     role: "user",
+      //     content: userInput,
+      //     // content: "Hello, I'm a human",
+      //   },
+      // ],
+    });
+
+    const answer = chatCompletion.data.choices[0].message?.content;
+    console.log(answer);
+
+    // 把机器人的回答添加到消息列表中
+    messages.push({
+      role: 'assistant',
+      content: answer || ''
+    })
+    console.log(messages)
+  }
 })();
+
+
